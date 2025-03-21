43f5a059b6da30d555d84011fc78ab0771b433ba
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2025-03-21 10:45:46 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import pygame
 turret_model= pygame.image.load("reverse_turret_120x120-removebg-preview.png")
 background = pygame.image.load("test_background.jpg")
 tower = pygame.image.load("tour-Photoroom.png")
