a2493f9d9e6a67c25f4cd632d66ac98f12150ecc
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2025-03-21 10:45:17 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,3 @@
+turret_model= pygame.image.load("reverse_turret_120x120-removebg-preview.png")
+background = pygame.image.load("test_background.jpg")
+tower = pygame.image.load("tour-Photoroom.png")
