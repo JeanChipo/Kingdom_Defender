75e7d4a6d83882aa1a726b252b5cf43b6959991e
A1NZ <159454904+Dragbnx@users.noreply.github.com>
2025-04-13 15:12:14 +0200

    mise en relation ennemis + tourelle

--- main.py
+++ main.py
@@ -21,14 +21,15 @@ CLOCK = pygame.time.Clock()
 NB_FPS = 60
 
 LONG_BANDEAU = 12.5
+turrets = Turret_Gestion()
 # menu_surface = pygame.Surface((WIDTH, HEIGHT))
 B_upg_tower = Button((230,230,230), (175, 175, 175), (150, 150, 150), (0, 0, 0), "upgrade tower", "kristenitc", 16, 
                  (100+LONG_BANDEAU, 40), (SCREEN.get_width()-150+LONG_BANDEAU, 100+LONG_BANDEAU), SCREEN.get_size(), lambda: print("x"), SCREEN)
 B_upg_turret = Button((230,230,230), (175, 175, 175), (150, 150, 150), (0, 0, 0), "upgrade turret", "kristenitc", 16, 
-                 (100+LONG_BANDEAU, 40), (SCREEN.get_width()-150+LONG_BANDEAU, 150+LONG_BANDEAU), SCREEN.get_size(), lambda: print("x"), SCREEN)
+                 (100+LONG_BANDEAU, 40), (SCREEN.get_width()-150+LONG_BANDEAU, 150+LONG_BANDEAU), SCREEN.get_size(), turrets.turrets[0].upgrade, SCREEN)
 BUTTON_LIST = [B_upg_tower, B_upg_turret]
 
-turrets = Turret_Gestion()
+
 
 wave_number = 1
 enemies, all_sprites = create_wave(wave_number,SCREEN.get_width(),420 * height_ratio() )
@@ -57,7 +58,6 @@ while RUNNING:
     SCREEN.blit(resize_background(background), (0, 0))
     SCREEN.blit(resize_tower(tower_1), (-100 * width_ratio() / 1.2,
                                         SCREEN.get_height() - 450 * height_ratio()))  # "colle" la tour en bas à gauche de la fenètre
-    turrets.turrets[0].update()
 
     if PAUSE :
         SCREEN.blit(pygame.font.Font(None, 48).render("PAUSED", True, "Black"),   (WIDTH//2 - 6*12, 10)) # 6 is the length of "PAUSED", 12 is the width of each character
@@ -66,7 +66,7 @@ while RUNNING:
     menu(SCREEN, (240,240,240), (SCREEN.get_width()-150, 100, 150-12.5, 300))
     for but in BUTTON_LIST:
         but.render(pygame.mouse.get_pos())
-    turrets.run(SCREEN, WIDTH)
+    turrets.run(SCREEN, SCREEN.get_width(), SCREEN.get_width(), enemies)
 
     texte_fps = POLICE.render(f"{int(CLOCK.get_fps())} FPS", True, "Black")
     SCREEN.blit(texte_fps, (10, 10))
@@ -74,6 +74,7 @@ while RUNNING:
     DT = NB_FPS / 10
 
     enemies, all_sprites,  wave_number = run_enemy(SCREEN, all_sprites, enemies, wave_number,turrets.turrets[0].get_bullet())
+
 
     pygame.display.flip()
 
