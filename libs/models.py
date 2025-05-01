0484c496129a6da93e616e3e9e27567566a7146f
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2025-05-01 18:02:31 +0200

    Update models.py

--- libs/models.py
+++ libs/models.py
@@ -2,5 +2,9 @@ import pygame
 
 turret_model= pygame.image.load("./assets/reverse_turret_120x120-removebg-preview.png")
 background = pygame.image.load("assets/test_background.jpg")
-tower_1 = pygame.image.load("assets/tour-Photoroom.png")
+tower_1 = pygame.image.load("assets/lvl_1_Tower_right_size.PNG")
+tower_2 = pygame.image.load("assets/lvl_2_Tower.png")
+tower_3 = pygame.image.load("assets/Tower_level_3.PNG")
+
 cannonball = pygame.image.load("assets/cannonball.png")
+baliste = pygame.image.load("assets/baliste_détourée_100by100.png")
